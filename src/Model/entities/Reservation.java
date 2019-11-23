a5697a351ee0ef50517ee4b1838c7868f6d47b67
Klayver Pricinote <klayverpricinote@hotmail.com>
2019-11-23 12:36:06 -0300

    Melhor

--- src/Model/entities/Reservation.java
+++ src/Model/entities/Reservation.java
@@ -1,5 +1,7 @@
 package Model.entities;
 
+import Model.excepetions.DomainException;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -44,13 +46,13 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public String updateDates(Date checkIn, Date checkOut){
+    public String updateDates(Date checkIn, Date checkOut) throws DomainException {
         Date now = new Date();
         if(checkIn.before(now) || checkOut.before(now)){
-            return "Error in reservation: Reservation dates for update must be future dates";
+            throw new DomainException("Error in reservation: Reservation dates for update must be future dates");
         }
         if (!checkOut.after(checkIn)){
-            return "Error in reservation: Check-out date must be after check-in date";
+            throw new DomainException("Error in reservation: Check-out date must be after check-in date");
         }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
